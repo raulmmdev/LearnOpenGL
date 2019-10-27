5a1080037d6396426a6be9ea66c4fec4a6948a01
Raul Mena <raulmenamontes@gmail.com>
2019-10-27 10:41:32 +0100

    Add camera with rotation and more cubes to render, replace

--- Source.cpp
+++ Source.cpp
@@ -189,21 +189,31 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		//camera
+		glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+		glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+		glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+		glm::vec3 up = glm::vec3(0.0, 1.0f, 0.0f);
+		glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+		glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
+		//rotate camera
+		float radius = 10.0f;
+		float camX = sin(glfwGetTime()) * radius;
+		float camZ = cos(glfwGetTime()) * radius;
+	
+		// projection matrix
 		glm::mat4 model = glm::mat4(1.0f);
-		//model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0, 0.0f, 0.0f));
 		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-		glm::mat4 view = glm::mat4(1.0f);
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0, camZ),
+			glm::vec3(0.0, 0.0, 0.0),
+			glm::vec3(0.0, 1.0, 0.0));
+
 		glm::mat4 projection;
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.f);
 
-		
-		int modelLoc = glGetUniformLocation(ourShader.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		int modelView = glGetUniformLocation(ourShader.ID, "view");
-		glUniformMatrix4fv(modelView, 1, GL_FALSE, glm::value_ptr(view));
-		int modelPro = glGetUniformLocation(ourShader.ID, "projection");
-		glUniformMatrix4fv(modelPro, 1, GL_FALSE, glm::value_ptr(projection));
+		// send to shader
+		ourShader.setMat4("view", view);
+		ourShader.setMat4("projection", projection);
 
 		glBindVertexArray(VAO);
 		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
