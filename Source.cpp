14bb3a6c787148f4751f51336dfec704e68b5468
Raul Mena <raulmenamontes@gmail.com>
2019-10-27 14:02:18 +0100

    Add values for camera settings therefore fix camera on firstMouse

--- Source.cpp
+++ Source.cpp
@@ -22,8 +22,11 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
  glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
  glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
  glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
- float lastX = 400, lastY = 300;
- float yaw, pitch;
+ // more settings
+ float lastX = (float)SCR_WIDTH / 2.0;
+ float lastY = (float)SCR_HEIGHT / 2.0;
+ float yaw = -90.0f;
+ float pitch= 0.0f;
  bool firstMouse = true;
  float fov = 45.0f;
 
