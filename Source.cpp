466eb3516e915b281d3359a3560754dc2804f22f
Raul Mena <raulmenamontes@gmail.com>
2019-10-30 17:36:50 +0100

    Add send settings values for struct material as individual uniforms

--- Source.cpp
+++ Source.cpp
@@ -152,7 +152,10 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 		ourShader.use();
-		ourShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		ourShader.setVec3("material.specular", 1.0f, 0.5f, 0.31f);
+		ourShader.setFloat("material.shininess", 32.0f);
 		ourShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		ourShader.setVec3("lightPos", lightPos);
 		ourShader.setVec3("viewPos", camera.Position);
