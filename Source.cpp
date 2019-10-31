7df600c6ae227d36a5f63af3a09bef206ed13c2f
Raul Mena <raulmenamontes@gmail.com>
2019-10-31 17:55:56 +0100

    Add load diffuse texture and change values of light and material

--- Source.cpp
+++ Source.cpp
@@ -142,7 +142,10 @@ int main()
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	unsigned int diffuseMap = loadTexture("container2.png");
 
+	ourShader.use();
+	ourShader.setInt("material.diffuse", 0);
 
 	
 	while (!glfwWindowShouldClose(window))
@@ -156,26 +159,13 @@ int main()
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
-		/*glm::vec3 lightColor;
-		lightColor.x = sin(glfwGetTime() * 2.0f);
-		lightColor.y = sin(glfwGetTime() * 0.7f);
-		lightColor.z = sin(glfwGetTime() * 1.3f);*/
-
-		// for decrease the influence
-		//glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-		// low influence
-		//glm::vec3 ambientcolor = diffuseColor * glm::vec3(0.2f);
-	
 		ourShader.use();
-		ourShader.setVec3("material.ambient", 0.0f, 1.5f, 0.06f);
-		ourShader.setInt("material.diffuse",0);
-		ourShader.setVec3("material.specular", 0.50196078f, 0.50196078f, 0.50196078f);
-		ourShader.setFloat("material.shininess", 32.0);
+		ourShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+		ourShader.setFloat("material.shininess", 64.0f);
 		//ourShader.setVec3("light.ambient", ambientcolor);
 		//ourShader.setVec3("light.diffuse", diffuseColor);
-		ourShader.setVec3("light.ambient", 1.0f, 1.0f, 1.0f);
-		ourShader.setVec3("light.diffuse", 1.0f, 1.0f, 1.0f);
+		ourShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+		ourShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
 		ourShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 		ourShader.setVec3("light.position", lightPos);
 		ourShader.setVec3("viewPos", camera.Position);
@@ -188,6 +178,10 @@ int main()
 		//world transformation
 		glm::mat4 model = glm::mat4(1.0f);
 		ourShader.setMat4("model", model);
+
+		//bind diffuse map
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
 
 		//render cube
 		glBindVertexArray(VAO);
