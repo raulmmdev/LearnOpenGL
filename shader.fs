73fa84af725f2a5668cf6b782785fbb6bdfdfb1e
Raul Mena <raulmenamontes@gmail.com>
2019-10-31 17:50:08 +0100

    Add rgb on texture vector

--- shader.fs
+++ shader.fs
@@ -26,13 +26,14 @@ in vec2 TexCoords;
 void main()
 {
 	// ambient
-	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	// TODO why add rgb on texture
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords)).rgb;
 
 	//diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm,lightDir), 0.0);
-	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords)).rgb;
 	
 	// specular
 	vec3 viewDir = normalize(viewPos - FragPos);
