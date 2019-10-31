731b37afbeece2d5c1b5a64744fa48cc0611c31f
Raul Mena <raulmenamontes@gmail.com>
2019-10-31 18:12:25 +0100

    Add specular texture to fragment shader

--- shader.fs
+++ shader.fs
@@ -1,7 +1,7 @@
 #version 330 core
 struct Material {
 	sampler2D diffuse;
-	vec3 specular;
+	sampler2D specular;
 	float shininess;
 };
 
@@ -26,22 +26,20 @@ in vec2 TexCoords;
 void main()
 {
 	// ambient
-	// TODO why add rgb on texture
-	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords)).rgb;
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
 	//diffuse
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm,lightDir), 0.0);
-	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords)).rgb;
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords));
 	
 	// specular
 	vec3 viewDir = normalize(viewPos - FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 	//calculate specular component
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-	vec3 specular =  light.specular * (spec * material.specular);
+	vec3 specular =  light.specular * spec * vec3(texture(material.specular, TexCoords));
 
-	vec3 result = ambient + diffuse + specular;
-    FragColor = vec4(result, 1.0);
+    FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
