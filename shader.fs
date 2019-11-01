2df111cc272cca6bb294536b9a5d7b0e0a1ee3f6
Raul Mena <raulmenamontes@gmail.com>
2019-11-01 14:52:04 +0100

    Add attenuation on fragment shader for Point lights

--- shader.fs
+++ shader.fs
@@ -6,10 +6,14 @@ struct Material {
 };
 
 struct Light {
-	vec3 direction;
+	vec3 position;
 	vec3 ambient;
 	vec3 diffuse;
 	vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
 };
 
 out vec4 FragColor;  
@@ -25,12 +29,16 @@ in vec2 TexCoords;
 
 void main()
 {
+// attenuation
+	float distance = length(light.position - FragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
 	// ambient
 	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
 
 	//diffuse
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(-light.direction);
+	vec3 lightDir = normalize(light.position - FragPos);
 	float diff = max(dot(norm,lightDir), 0.0);
 	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse,TexCoords));
 	
@@ -40,6 +48,10 @@ void main()
 	//calculate specular component
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
 	vec3 specular =  light.specular * spec * (vec3(1.0) - vec3(texture(material.specular, TexCoords)));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
 
     FragColor = vec4(ambient + diffuse + specular, 1.0);
 }
