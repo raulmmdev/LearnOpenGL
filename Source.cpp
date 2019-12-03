a251d6756537de592c5961c3bdc70cba59f0b1a2
Raul Mena <raulmenamontes@gmail.com>
2019-12-03 18:47:22 +0100

    Add enable blending

--- Source.cpp
+++ Source.cpp
@@ -67,6 +67,9 @@ int main()
 	glStencilFunc(GL_NOTEQUAL, 1, 0xFF);
 	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 	Shader ourShader("shader.vs", "shader.fs");
 	Shader shaderSingleColor("shader.vs", "ShaderSingleColor.fs");
 
