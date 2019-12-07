9b2b409713ed05fc0a7438f083933a8849bd6724
Raul Mena <raulmenamontes@gmail.com>
2019-12-07 11:53:40 +0100

    Refraction env mapping setting ratio from air to glass

--- shader.fs
+++ shader.fs
@@ -9,6 +9,7 @@ uniform samplerCube skybox;
 
 void main()
 {    
+	float ratio = 1.00 / 1.52;
     vec3 I = normalize(Position - cameraPos);
     vec3 R = reflect(I, normalize(Normal));
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
