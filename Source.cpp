de69abc9c0133f7ec53236c739f1877d7a8bf7ac
Raul Mena <raulmenamontes@gmail.com>
2019-12-09 16:22:24 +0100

    Change vertex data for add color and change vertex attribute pointer

--- Source.cpp
+++ Source.cpp
@@ -67,10 +67,10 @@ int main()
 	Shader ourShader("shader.vs", "shader.fs", "shader.gs");
 
 	float points[] = {
-		-0.5f,  0.5f, // top-left
-		 0.5f,  0.5f, // top-right
-		 0.5f, -0.5f, // bottom-right
-		-0.5f, -0.5f  // bottom-left
+		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // top-left
+		 0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // top-right
+		 0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // bottom-right
+		-0.5f, -0.5f, 1.0f, 1.0f, 0.0f  // bottom-left
 	};
 
 	// cube VAO
@@ -81,7 +81,10 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2* sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+	glBindVertexArray(0);
 
 	unsigned int uboMatrices;
 	glGenBuffers(1, &uboMatrices);
