78ca940d179ddecf8180e42094f3f547bb278a63
Raul Mena <raulmenamontes@gmail.com>
2019-12-09 14:57:03 +0100

    Change fs to render on green all points

--- shader.fs
+++ shader.fs
@@ -1,17 +1,8 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
-in vec3 Position;
-
-uniform vec3 cameraPos;
-uniform samplerCube skybox;
-
 void main()
 {    
-	float ratio = 1.00 / 1.52;
-    vec3 I = normalize(Position - cameraPos);
-    vec3 R = reflect(I, normalize(Normal));
-    FragColor = vec4(texture(skybox, R).rgb, 1.0);
+	FragColor = vec4(0.0, 1.0, 0.0, 1.0);
 }
 
